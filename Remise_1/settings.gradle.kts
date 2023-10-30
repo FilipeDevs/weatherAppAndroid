c4fa1c1bf156ed93ccda98abc19afdc64fe05ff0
Filipe Pereira <filipe.pereira.dev@gmail.com>
2023-10-30 15:37:22 +0100

    Setup Projet - Remise 1

---
+++ Remise_1/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Remise_1"
+include(":app")
+ 
