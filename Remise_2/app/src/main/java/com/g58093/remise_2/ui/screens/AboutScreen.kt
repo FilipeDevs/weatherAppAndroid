e4153f430774f2f84166d2184b33e73006ca0cf7
Filipe Pereira <filipe.pereira.dev@gmail.com>
2023-11-20 09:40:05 +0100

    Add strings

--- Remise_2/app/src/main/java/com/g58093/remise_2/ui/screens/AboutScreen.kt
+++ Remise_2/app/src/main/java/com/g58093/remise_2/ui/screens/AboutScreen.kt
@@ -8,9 +8,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.g58093.remise_2.R
 
 @Composable
 fun AboutScreen(modifier: Modifier) {
@@ -22,18 +24,18 @@ fun AboutScreen(modifier: Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "Filipe Pereira",
+            text = stringResource(R.string.myName),
             fontWeight = FontWeight.Bold,
             fontSize = 20.sp,
             modifier = Modifier.padding(bottom = 8.dp)
         )
         Text(
-            text = "58093",
+            text = stringResource(R.string.matricule),
             fontSize = 18.sp,
             modifier = Modifier.padding(bottom = 8.dp)
         )
         Text(
-            text = "E11",
+            text = stringResource(R.string.groupe),
             fontSize = 18.sp
         )
     }
